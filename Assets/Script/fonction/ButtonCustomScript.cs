e10f34b9ee089aa1c17e2e93c6eaf0799d2893bc
TheChasim <122937477+TheChasim@users.noreply.github.com>
2024-07-25 17:30:30 -0400

    ajout du level up

---
+++ Assets/Script/fonction/ButtonCustomScript.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+using System.Runtime.CompilerServices;
+using System;
+
+public class ButtonCustomScript : MonoBehaviour, IPointerClickHandler
+{
+    [Header("Size Setting")]
+    [SerializeField] Vector3 minSize;
+    [SerializeField] Vector3 maxSize;
+    [SerializeField] Vector3 sizeIncreasement = new Vector3(0.5f, 0.5f, 0.5f);
+
+    [Header("Stat")]
+    [SerializeField] bool Power = false;
+    [SerializeField] bool Range = false;
+    [SerializeField] bool Coldown = false;
+
+    [SerializeField] LevelUpSystem levelUpSystem;
+
+
+
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            if (Power)
+            {
+                UpSize();
+            }
+            else if (Range)
+            {
+
+                UpSize();
+            }
+            else if (Coldown)
+            {
+                UpSize();
+
+            }
+        }
+
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            if (Power)
+            {
+                DownSIze();
+            }
+            else if (Range)
+            {
+                DownSIze();
+
+            }
+            else if (Coldown)
+            {
+                DownSIze();
+
+            }
+        }
+
+
+    }
+
+    private void UpSize()
+    {
+        if (transform.localScale.x + sizeIncreasement.x <= maxSize.x)
+        {
+            transform.localScale = transform.localScale + sizeIncreasement;
+        }
+
+    }
+
+    private void DownSIze()
+    {
+        if (transform.localScale.x - sizeIncreasement.x >= minSize.x)
+        {
+            transform.localScale = transform.localScale - sizeIncreasement;
+        }
+
+    }
+
+}
+
---
+++ Assets/Script/fonction/LevelUpSystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpSystem : MonoBehaviour
+{
+
+
+    public void OnPowerUp()
+    {
+
+        Debug.Log("Power up");
+
+    }
+
+    public void OnRangeUp()
+    {
+        Debug.Log("Range Up");
+    }
+
+    public void OnColdownUp()
+    {
+        Debug.Log("Coldown Up");
+    }
+
+
+}
