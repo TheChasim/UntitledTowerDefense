8bb07dffdc70c3e24e3f986f264368b87c73f404
TheChasim <122937477+TheChasim@users.noreply.github.com>
2025-02-20 15:13:28 -0500

    retire les ancien scripte de path finding

--- Assets/Script/Player/InputManager.cs
+++ Assets/Script/Player/InputManager.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] MovingCam cam;
 
-    // Update is called once per frame
-    void Update()
+    public void OnZoom(InputAction.CallbackContext context)
     {
-        
+        cam.OnZoom(context);
     }
 }
--- Assets/Script/Player/MovingCam.cs
+++ Assets/Script/Player/MovingCam.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class MovingCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float zoomSensitivity = 0.5f;
+    [SerializeField] float minZoom = 1;
+    [SerializeField] float maxZoom = 10;
+    float zoomValue;
+
+    GameObject cam;
+
+    private void Start()
     {
-        
+        cam = FindObjectOfType<Camera>().gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleZoom();
+
+    }
+
+    private void HandleZoom()
+    {
+        cam.GetComponent<Camera>().orthographicSize += zoomValue;
+
+        cam.GetComponent<Camera>().orthographicSize = Mathf.Clamp(cam.GetComponent<Camera>().orthographicSize, minZoom, maxZoom);
+    }
+
+    public void OnZoom(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            zoomValue = context.ReadValue<Vector2>().y* zoomSensitivity;
+        }
+        if(context.canceled)
+        {
+            zoomValue = 0;
+        }
     }
 }
